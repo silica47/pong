ddbf26d0eabf58108607e6c824376a96c7f2af0f
dogemon <techie-human@protonmail.com>
2022-04-04 13:08:01 +0530

    increased font size

--- main.lua
+++ main.lua
@@ -10,7 +10,7 @@ function love.load()
     ball:load()
     bot:load()
     score = {player = 0, bot = 0}
-    font = love.graphics.newFont(50)
+    font = love.graphics.newFont(40)
 end
 
 function love.update(dt)
@@ -23,10 +23,14 @@ function love.draw()
     player:draw()
     ball:draw()
     bot:draw()
+    draw_score()
+end
+
+function draw_score()
+    love.graphics.setFont(font)
     love.graphics.print("Score: "..score.player, 50,50)
     love.graphics.print("Score: "..score.bot, 1000,50)
 end
-
 --checking collision between bodies
 function collision(a, b)
     if a.x + a.width > b.x and a.x < b.x + b.width and a.y + a.height > b.y and a.y < b.y + b.height then
